ff285c1b332991a4e69b173f56d41ed55514e83c
Stephen17C <markstephenseril@gmail.com>
2025-03-12 00:10:56 +0800

    Add files via upload

---
+++ lab act 4.py
@@ -0,0 +1,5 @@
+check_number = int(input("Give me a number and I will tell you to find out even and odd numbers:"))
+if check_number % 100 == 0:
+	print( "it is an even number.")
+else:
+	print(" it is an odd number.")
